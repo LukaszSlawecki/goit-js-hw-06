2a963744e24c146c473f9c26c8d3ccf863705049
Łukasz Sławecki <111075603+LukaszSlawecki@users.noreply.github.com>
2023-11-06 21:52:05 +0100

    koniec
    koniec

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const bodyEl = document.querySelector("body");
+const buttonEl = document.querySelector(".change-color");
+const colorName = document.querySelector(".color");
+
+buttonEl.addEventListener("click", () => {
+  const mainColor = getRandomHexColor();
+
+  bodyEl.style.backgroundColor = mainColor;
+
+  colorName.textContent = mainColor;
+});
